3609f414220947de1b02f08b7fa4e914a4051408
Draedon123 <draedon123@gmail.com>
2024-02-17 12:20:26 +1000

    Downgraded PIXI.js to 7.1.1 and PIXI-Viewport to 5.0.1

--- ts/script.ts
+++ ts/script.ts
@@ -3,7 +3,7 @@ import { Application, Sprite, Texture } from 'pixi.js';
 
 const canvas = document.querySelector("canvas")!;
 
-const app = new Application({view: canvas, autoDensity: true, antialias: true, sharedTicker: true, autoStart: false});
+const app = new Application({view: canvas, autoDensity: true, antialias: true, sharedTicker: true, autoStart: false, hello: true});
 const viewport = new Viewport({events: app.renderer.events, passiveWheel: false, screenHeight: canvas.offsetHeight, screenWidth: canvas.offsetWidth});
 viewport.drag({}).decelerate({minSpeed: 0.3, friction: 0.96}).pinch({}).wheel({});
 
